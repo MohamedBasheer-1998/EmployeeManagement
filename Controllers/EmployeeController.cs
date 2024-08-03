8a43773c8e947069a74e5035f6988dd2c932e544
Mohamed Basheer <basheer@buckman.com>
2024-08-03 13:31:54 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private static readonly List<Employee> employees = new List<Employee>();
+
+        public ActionResult Index()
+        {
+            return View(employees);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                employee.Id = employees.Count + 1;
+                employees.Add(employee);
+                return RedirectToAction("Index");
+
+            }
+
+                return View(employee);
+            }
+        }
+    }
+
---
+++ Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string Position { get; set; }
+        public string Address { get; set; }
+    }
+}
